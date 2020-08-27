c79bf98b114558115b4720eb4a52053dd9b75eee
Celina Jackson <celinanjackson@gmail.com>
2020-08-27 23:08:41 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,6 +1,12 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
 
   def change 
+    create_table :artists do |t|
+      t.string :name 
+      t.string :genre 
+      t.string :age
+      t.string :hometown 
+    end 
   end 
   
   def up
